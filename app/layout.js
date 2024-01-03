0279af12de52559ceb704742fed8ee25daa55319
siddak18 <siddakrajpal14@gmail.com>
2024-01-03 13:27:31 +0530

    impcor

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,6 @@
 
 import { Inter } from 'next/font/google'
-import './globals.css'
+import '../Styles/globals.css'
 import Navbar from '../Components/Navbar'
 import Footer from '@/Components/Footer'
 
